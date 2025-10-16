5fff02256f948b702a0b46e81b6a16aa0e79939b
Arismendy Gill <arismendygill@icloud.com>
2025-10-16 19:23:57 -0400

    Initial Commit

---
+++ football/footballApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballApp.swift
+//  football
+//
+//  Created by AG on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct footballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
